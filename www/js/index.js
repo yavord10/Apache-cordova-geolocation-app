22ef07bb74e81b6ee6a2f4752744e1274dab6e55
yavord10 <yavor.v.dimitrov@gmail.com>
2021-06-07 12:18:54 +0800

    add orders to list

--- www/js/index.js
+++ www/js/index.js
@@ -6,12 +6,16 @@ document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
     console.log("Running cordova-" + cordova.platformId + "@" + cordova.version);
-    // Create the TaxiShare object for use by the HTML view
+
     controller = new Controller();
-    
+
+    // FR 1.2 ---> display widgets
     controller.displayWidgetData();
 
-    controller.validateInput();
+    // FR 1.1 ---> validate and handle all input
+    controller.handleInput();
+
+    controller.getClientData();
 }
 
 
@@ -21,6 +25,11 @@ function Controller() {
 
     let currentlyDisplayedWidget = 1;
     let oucu = 'yvd2';
+    let clientId = 1;
+    let orderAmount = 0;
+    let agreedPrice = 0;
+    let orderId = "";
+    let orderList = [];
 
     const fetchData = async (dataType, id='') => {
         const res = await fetch(`${BASE_URL}${dataType}/${id}?OUCU=${oucu}&password=${API_PASSWORD}`);
@@ -48,8 +57,8 @@ function Controller() {
         return json.data;
     };
 
-    //VALIDATE INPUT ---> FR1.1
-    this.validateInput = () => {
+    //  FR1.1 ---> VALIDATE INPUT
+    this.handleInput = () => {
         const validateOUCU = (oucu) => {
             console.log(oucu, oucu.charAt(0), oucu.charAt(oucu.length -1));
             if (oucu.charAt(0).match(/[a-z]/i) && /^\d+$/.test(oucu.charAt(oucu.length - 1))) {
@@ -71,11 +80,23 @@ function Controller() {
                     message.textContent = 'OUCU is invalid';
                 }
             }
+
+            if (e.target.id === 'clientId') {
+                clientId = e.target.value;
+            }
+
+            if (e.target.id === 'orderAmount') {
+                console.log(e.target.value);
+                orderAmount = e.target.value;
+            }
+
+            if (e.target.id === 'agreedPrice') {
+                agreedPrice = e.target.value;
+            }
         })
     }
 
-
-    //WIDGETS ---> FR1.2
+    //  FR1.2 ----> DISPLAY WIDGETS
     const displayWidget = (widgetData) => {
         console.log(widgetData);
 
@@ -84,6 +105,10 @@ function Controller() {
 
         const img = document.getElementById('widgetImage');
         img.src = widgetData[0].url;
+
+        const price = document.getElementById('agreedPrice');
+        price.value = widgetData[0].pence_price;
+        agreedPrice = widgetData[0].pence_price;
     };
 
     this.displayWidgetData = async () => {
@@ -107,4 +132,64 @@ function Controller() {
 
         currentlyDisplayedWidget = currentlyDisplayedWidget - 1;
     };
+
+    this.getClientData = async () => {
+        const clientData = await fetchData('clients', clientId);
+        console.log(clientData);
+    }
+
+
+    // FR 1.3 ---> ADDING WIDGET TO ORDER ITEMS
+    this.createOrder = () => {
+        fetch(`${BASE_URL}orders?OUCU=tm352&password=`, {
+            method: 'POST',
+            body: JSON.stringify({
+                OUCU: oucu,
+                password: API_PASSWORD,
+                client_id: clientId,
+                latitude: 20,
+                longitude: -15
+            })
+        })
+        .then((res) => res.json())
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err))
+    }
+
+    this.getOrderItemsData = async () => {
+        const res = await fetch(`${BASE_URL}order_items?OUCU=${oucu}&password=${API_PASSWORD}`);
+        const json = await res.json();
+
+        if (json.data) {
+            json.data.forEach((order) => {
+                const listEl = document.getElementById('orderList');
+                const el = document.createElement('li');
+                const totalPrice = order.number * order.pence_price;
+                el.textContent = `${order.number} widget(s) with id ${order.widget_id} x ${order.pence_price} agreed price = ${totalPrice + 20 / 100 * totalPrice} inc VAT`
+                
+                listEl.appendChild(el);
+            })
+        }
+    }
+
+    this.addToOrder = () => {
+        const listEl = document.getElementById('orderList');
+        const el = document.createElement('li');
+        const totalPrice = orderAmount * agreedPrice;
+        el.textContent = `${orderAmount} widget(s) with id ${currentlyDisplayedWidget} x ${agreedPrice} agreed price = ${totalPrice + 20 / 100 * totalPrice} inc VAT`
+        
+        orderList = [...orderList, {
+            widgetId: currentlyDisplayedWidget,
+            amount: orderAmount,
+            price: agreedPrice,
+            total: totalPrice + 20 / 100 * totalPrice
+        }]
+        listEl.appendChild(el);
+    }
+
+    // FR 1.3 ---> start order
+    this.placeNewOrder = () => {
+        controller.createOrder();
+        controller.getOrderItemsData();
+    }
 }
